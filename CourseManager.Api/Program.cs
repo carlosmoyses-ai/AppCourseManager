95636d9a95bb939011c3dcc2d59cd24923a62d7c
Carlos Moyses <carlosmoyses@gmail.com>
2023-09-12 01:48:46 -0300

    Confiurado a iniciação dos controllers e do Swagger

--- CourseManager.Api/Program.cs
+++ CourseManager.Api/Program.cs
@@ -1,19 +1,34 @@
+using CourseManager.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppCourseManagerDbContext>();
+
+builder.Services.AddControllersWithViews();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppCourseManagerDbContext>();
+    DbInitializer.Initialize(context);
+}
+
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.UseRouting();
 
 app.Run();
