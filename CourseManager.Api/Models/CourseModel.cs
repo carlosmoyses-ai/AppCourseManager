1167f57ae285da808af01454787ae26cd5a50ca1
Carlos Moyses <carlosmoyses@gmail.com>
2023-09-12 01:45:59 -0300

    Atualizando as Classes Course e Student, e mudando de Collection para List

--- CourseManager.Api/Models/CourseModel.cs
+++ CourseManager.Api/Models/CourseModel.cs
@@ -1,13 +1,35 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
 namespace CourseManager.Api.Models;
 
 public class CourseModel
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Display(Name = "Id do Curso")]
     public int CourseId { get; set; }
-    public string? CourseName { get; set; }
-    public string? Description { get; set; }
-    public DateTime StartDate { get; set; }
-    public DateTime EndDate { get; set; }
-    public ICollection<StudentModel>? Students { get; internal set; }
 
+    [Required]
+    [MaxLength(200)]
+    [Display(Name = "Nome do Curso", Prompt = "Insira o nome do curso")]
+    public string? CourseName { get; set; }
     
+    [MaxLength(500)]
+    [Display(Name = "Descrição do Curso", Prompt = "Insira a descrição do curso")]
+    public string? Description { get; set; }
+
+    [Required]
+    [DataType(DataType.Date, ErrorMessage = "Insira uma data válida no formato dd/mm/yyyy")]
+    [Display(Name = "Data de Início", Prompt = "Insira a data de início do curso")]
+    public DateTime StartDate { get; set; }
+    [Required]
+    [DataType(DataType.Date, ErrorMessage = "Insira uma data válida no formato dd/mm/yyyy")]
+    [Display(Name = "Data de Término", Prompt = "Insira a data de término do curso")]
+    public DateTime EndDate { get; set; }
+
+    [JsonIgnore]
+    [Display(Name = "Alunos Matriculados", Prompt = "Insira os alunos matriculados no curso")]
+    public List<StudentModel>? Students { get; set; } = new();
 }
--- CourseManager.Api/Models/StudentModel.cs
+++ CourseManager.Api/Models/StudentModel.cs
@@ -1,11 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
 namespace CourseManager.Api.Models;
 
 public class StudentModel
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Display(Name = "Id do Aluno")]
     public int StudentId { get; set; }
+
+    [Required]
+    [MaxLength(200)]
+    [Display(Name = "Nome do Aluno", Prompt = "Insira o nome do aluno")]
     public string? StudentName { get; set; }
+
+    [Required]
+    [EmailAddress(ErrorMessage = "Endereço de email inválido")]
+    [MaxLength(200)]
+    [Display(Name = "Email", Prompt = "Insira o email do aluno")]
     public string? Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Date, ErrorMessage = "Insira uma data válida no formato dd/mm/yyyy")]
+    [Display(Name = "Data de Matrícula", Prompt = "Insira a data de matrícula do aluno")]
     public DateTime EnrollmentDate { get; set; }
 
-    public ICollection<CourseModel>? Courses { get; internal set; }
+    [JsonIgnore]
+    [Display(Name = "Cursos Matriculados", Prompt = "Insira os cursos matriculados pelo aluno")]
+    public List<CourseModel>? Courses { get; set; } = new();
 }
