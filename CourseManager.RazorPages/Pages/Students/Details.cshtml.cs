b4658a38447b9c7e54c5a24fa6b94dcf7ef13ffd
Carlos Moyses <carlosmoyses@gmail.com>
2023-09-12 11:54:36 -0300

    Subindo Razor Page

--- CourseManager.RazorPages/Pages/Students/Details.cshtml.cs
+++ CourseManager.RazorPages/Pages/Students/Details.cshtml.cs
@@ -9,16 +9,20 @@ public class Details : PageModel
 {
     [BindProperty]
     public StudentModel Student { get; set; } = new();
-    public List<CourseModel> Courses { get; set; } = new();
+    public IList<CourseModel> Courses { get; set; } = new List<CourseModel>();
 
     public async Task<IActionResult> OnGetAsync(int id)
     {
         Uri uri = new($"http://localhost:5259/api/Student/GetStudent/{id}");
+        Uri uri2 = new(uri.OriginalString + "/Courses");
         using HttpResponseMessage response = await new HttpClient().GetAsync(uri);
+        using HttpResponseMessage response2 = await new HttpClient().GetAsync(uri2);
         if (response.IsSuccessStatusCode)
         {
             string json = await response.Content.ReadAsStringAsync();
+            string json2 = await response2.Content.ReadAsStringAsync();
             Student = JsonConvert.DeserializeObject<StudentModel>(json) ?? new();
+            Courses = JsonConvert.DeserializeObject<List<CourseModel>>(json2) ?? new();
             return Page();
         }
 
