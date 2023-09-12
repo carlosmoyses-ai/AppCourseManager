daa216a93794f4ebb420c760cea6730123bde39d
Carlos Moyses <carlosmoyses@gmail.com>
2023-09-12 01:51:20 -0300

    Confiurado as tabelas Students e Courses e a tabela de muito para muitos, e o arquivo do banco sqlite

--- CourseManager.Api/Data/AppCourseManagerDbContext.cs
+++ CourseManager.Api/Data/AppCourseManagerDbContext.cs
@@ -8,21 +8,78 @@ public class AppCourseManagerDbContext : DbContext
     {
     }
 
-    public DbSet<CourseModel> Courses { get; set; } = default!;
-    public DbSet<StudentModel> Students { get; set; } = default!;
+    public DbSet<StudentModel> Students { get; set; }
+    public DbSet<CourseModel> Courses { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlite("Data Source=.\\DataBase\\coursemanager.db");
+        optionsBuilder.UseSqlite("Data Source=.\\DataBase\\CourseManager.db");
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<CourseModel>()
-            .HasMany(c => c.Students)
-            .WithMany(s => s.Courses);
+            .ToTable("Courses")
+            .HasKey(c => c.CourseId);
+
+        modelBuilder.Entity<CourseModel>()
+            .Property(c => c.CourseId)
+            .ValueGeneratedOnAdd();
+
         modelBuilder.Entity<StudentModel>()
-            .HasMany(s => s.Courses)
-            .WithMany(c => c.Students);
+            .ToTable("Students")
+            .HasKey(a => a.StudentId);
+
+        modelBuilder.Entity<StudentModel>()
+            .Property(a => a.StudentId)
+            .ValueGeneratedOnAdd();
+
+        modelBuilder.Entity<CourseModel>()
+            .HasMany(c => c.Students)
+            .WithMany()
+            .UsingEntity<Dictionary<string, object>>(
+                "CourseStudent",
+                s => s
+                    .HasOne<StudentModel>()
+                    .WithMany()
+                    .HasForeignKey("StudentId")
+                    .HasConstraintName("FK_CourseStudent_Students_StudentId")
+                    .OnDelete(DeleteBehavior.Cascade),
+                c => c
+                    .HasOne<CourseModel>()
+                    .WithMany()
+                    .HasForeignKey("CourseId")
+                    .HasConstraintName("FK_CourseStudent_Courses_CourseId")
+                    .OnDelete(DeleteBehavior.Cascade),
+                sC =>
+                {
+                    sC.HasKey("CourseId", "StudentId");
+                    sC.ToTable("CourseStudent");
+                }
+            );
+
+        modelBuilder.Entity<StudentModel>()
+            .HasMany(a => a.Courses)
+            .WithMany()
+            .UsingEntity<Dictionary<string, object>>(
+                "CourseStudent",
+                c => c
+                    .HasOne<CourseModel>()
+                    .WithMany()
+                    .HasForeignKey("CourseId")
+                    .HasConstraintName("FK_CourseStudent_Courses_CourseId")
+                    .OnDelete(DeleteBehavior.Cascade),
+                s => s
+                    .HasOne<StudentModel>()
+                    .WithMany()
+                    .HasForeignKey("StudentId")
+                    .HasConstraintName("FK_CourseStudent_Students_StudentId")
+                    .OnDelete(DeleteBehavior.Cascade),
+                cS =>
+                {
+                    cS.HasKey("CourseId", "StudentId");
+                    cS.ToTable("CourseStudent");
+                }
+            );
     }
 }
